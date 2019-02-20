38c1ca57060c48c9fa34bc93368fadc4bacbf34c
gonzalo espinoza <gonis1990@gmail.com>
2019-02-19 22:35:51 -0400

    navbar router login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route  } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
+
+
+
+import Navbar from './component/navbar'
+import Login from './login/login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <header className="App-header">
+            
+            <Route exact path="/login" render={()=><Login onClickF={this.asignarUsuarioState}/>}/>
           
-         
-        </header>
-      </div>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
